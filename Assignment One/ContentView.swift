39cc3817429c72ae4fcc3c223f275dc897b1eb42
Colton Swingle <coltonswingle@gmail.com>
2021-01-16 23:39:45 -0800

    Initial commit with assets

--- Assignment One/ContentView.swift
+++ Assignment One/ContentView.swift
@@ -7,7 +7,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello CS342!")
+        
+        VStack {
+            Picture()
+            
+            Text("Paragraph text test. I'm a senior at Stanford University currently doing some interesting stuff ish. It would be hella cool if I could figure out how to write this and make it make sense.")
+                .font(.body)
+                .fontWeight(.medium)
+                .padding()
+        }
+        .offset(y: -150)
+        
     }
 }
 

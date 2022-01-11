76c6a7594cdb0adb124727e234033d8a7c6ccf76
Lavender Chen <lavenderchen@DN0a226450.SUNet>
2022-01-11 15:46:55 -0800

    Updated Bio

--- Assignment One/ContentView.swift
+++ Assignment One/ContentView.swift
@@ -7,10 +7,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello CS342!")
+        VStack() {
+            Text("Lavender Chen")
+                .bold()
+                .font(.title)
+            Image("Lavender")
+                .resizable()
+                .frame(width: 250, height: 250)
+        Button(action: {
+            }, label: {
+                    Link("Notion", destination: URL(string: "https://lavenderchen.notion.site/498a21745b8d427cb49e28ce1ce78b51?v=b5c0cc17763f4bca806a5c9562596344")!)
+                })
+        Divider()
+            Text("Lavender (Zi-Xun) Chen was born in 2002 in Taipei, Taiwan. She then immigrated to America at 1-year-old to San Francisco, California. She is a second-year undergraudate student, currently studying Symbolic Systems with a concentration in Human-Computer-Interaction at Stanford University. In her free time, she enjoys watching K-drama and BTS, baking, and traveling.")
+                .font(.headline)
+                .fontWeight(.regular)
+                .padding(.horizontal)
+        }
+        .padding(.leading)
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()

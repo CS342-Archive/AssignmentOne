9d69a8404b44fb13390c4cd7fc680f33af506ee5
Taylor Lallas <tlallas@stanford.edu>
2022-01-11 22:50:44 -0800

    progress on initial layout

---
+++ Assignment One/LongFormText.swift
@@ -0,0 +1,20 @@
+//
+//  LongFormText.swift
+//  Assignment One
+//
+//  Created by Taylor  Lallas on 1/11/22.
+//
+
+import SwiftUI
+
+struct LongFormText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LongFormText_Previews: PreviewProvider {
+    static var previews: some View {
+        LongFormText()
+    }
+}

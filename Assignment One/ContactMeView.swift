901c47fb41435e436b8e5481534f52b8b3d2ceed
Taylor Lallas <tlallas@stanford.edu>
2022-01-17 10:54:44 -0800

    finished assignment 1

---
+++ Assignment One/ContactMeView.swift
@@ -0,0 +1,20 @@
+//
+//  ContactMeView.swift
+//  Assignment One
+//
+//  Created by Taylor  Lallas on 1/13/22.
+//
+
+import SwiftUI
+
+struct ContactMeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContactMeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactMeView()
+    }
+}
---
+++ Assignment One/HighlightReel.swift
@@ -0,0 +1,20 @@
+//
+//  HighlightReel.swift
+//  Assignment One
+//
+//  Created by Taylor  Lallas on 1/13/22.
+//
+
+import SwiftUI
+
+struct HighlightReel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HighlightReel_Previews: PreviewProvider {
+    static var previews: some View {
+        HighlightReel()
+    }
+}

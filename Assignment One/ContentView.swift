dd4eed5a0a8d122c56a6a73dac7dac7092810072
Lavender Chen <lavenderchen@DNa1de473.SUNet>
2022-01-17 20:35:34 -0800

    Updated formatting to be centered

--- Assignment One/ContentView.swift
+++ Assignment One/ContentView.swift
@@ -12,21 +12,21 @@ struct ContentView: View {
             .font(.system(.title, design: .rounded))
             .bold()
             .foregroundColor(.purple)
-            .padding(.leading, 63)
+            .multilineTextAlignment(.center)
         Image("bio_image")
             .resizable()
             .scaledToFit()
         Button(action: {
             }, label: {
         Link("Get to know me!", destination: URL(string: "https://lavenderchen.notion.site/498a21745b8d427cb49e28ce1ce78b51?v=b5c0cc17763f4bca806a5c9562596344")!)
-                .padding(.leading, 110)})
+                .multilineTextAlignment(.center)})
         Divider()
         Text("Lavender (Zi-Xun) Chen was born in 2002 in Taipei, Taiwan. She then immigrated to America at 1-year-old to San Francisco, California. She is a second-year undergraudate student, currently studying Symbolic Systems with a concentration in Human-Computer-Interaction at Stanford University. In her free time, she enjoys watching K-drama and BTS, baking, and traveling.")
             .font(.headline)
             .fontWeight(.regular)
             .padding(.bottom)
         }
-        .padding(.leading)
+        .padding(.horizontal)
     }
 }
 struct ContentView_Previews: PreviewProvider {

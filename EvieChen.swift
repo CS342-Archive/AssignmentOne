cc8e5678788b875a7509b107ad4976800b4d7f0c
Evie Chen <eviechen@DN51vt2h.SUNet>
2022-01-18 00:16:14 -0800

    finished assignment 1

---
+++ EvieChen.swift
@@ -0,0 +1,20 @@
+//
+//  EvieChen.swift
+//  Assignment One
+//
+//  Created by Evie Chen on 2022/1/17.
+//
+
+import SwiftUI
+
+struct EvieChen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EvieChen_Previews: PreviewProvider {
+    static var previews: some View {
+        EvieChen()
+    }
+}

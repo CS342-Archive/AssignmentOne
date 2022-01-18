82c1c0938102705b9161ba1352ac407a60f1292a
Lavender Chen <lavenderchen@DNa1de473.SUNet>
2022-01-17 16:41:17 -0800

    Updated bio pic and layout

--- Assignment One/ContentView.swift
+++ Assignment One/ContentView.swift
@@ -8,21 +8,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Lavender Chen")
-                .bold()
-                .font(.title)
-            Image("Lavender")
-                .resizable()
-                .frame(width: 250, height: 250)
+        Text("Lavender Chen".uppercased())
+            .font(.system(.title, design: .rounded))
+            .bold()
+            .foregroundColor(.purple)
+            .padding(.leading, 63)
+        Image("bio_image")
+            .resizable()
+            .scaledToFit()
         Button(action: {
             }, label: {
-                    Link("Notion", destination: URL(string: "https://lavenderchen.notion.site/498a21745b8d427cb49e28ce1ce78b51?v=b5c0cc17763f4bca806a5c9562596344")!)
-                })
+        Link("Get to know me!", destination: URL(string: "https://lavenderchen.notion.site/498a21745b8d427cb49e28ce1ce78b51?v=b5c0cc17763f4bca806a5c9562596344")!)
+                .padding(.leading, 110)})
         Divider()
-            Text("Lavender (Zi-Xun) Chen was born in 2002 in Taipei, Taiwan. She then immigrated to America at 1-year-old to San Francisco, California. She is a second-year undergraudate student, currently studying Symbolic Systems with a concentration in Human-Computer-Interaction at Stanford University. In her free time, she enjoys watching K-drama and BTS, baking, and traveling.")
-                .font(.headline)
-                .fontWeight(.regular)
-                .padding(.horizontal)
+        Text("Lavender (Zi-Xun) Chen was born in 2002 in Taipei, Taiwan. She then immigrated to America at 1-year-old to San Francisco, California. She is a second-year undergraudate student, currently studying Symbolic Systems with a concentration in Human-Computer-Interaction at Stanford University. In her free time, she enjoys watching K-drama and BTS, baking, and traveling.")
+            .font(.headline)
+            .fontWeight(.regular)
+            .padding(.bottom)
         }
         .padding(.leading)
     }

ea3297c97921554cfbcbf56b2df46c1c688f9baf
Beste Aydin <bestea@blkw-cl-03-ims2.stanford.edu>
2022-01-18 19:52:57 -0800

    Beste's Assignment

---
+++ BesteAydin.swift
@@ -0,0 +1,20 @@
+//
+//  BesteAydin.swift
+//  Assignment One
+//
+//  Created by Beste Aydin on 1/18/22.
+//
+
+import SwiftUI
+
+struct BesteAydin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BesteAydin_Previews: PreviewProvider {
+    static var previews: some View {
+        BesteAydin()
+    }
+}

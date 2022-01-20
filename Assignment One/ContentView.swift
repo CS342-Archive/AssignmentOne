9b18906e93f16bcf68b5bd286f40e5c38623ea47
Surabhi <surabhi624@gmail.com>
2022-01-19 16:42:37 -0800

    added to list view

--- Assignment One/ContentView.swift
+++ Assignment One/ContentView.swift
@@ -36,13 +36,99 @@ struct ContentView: View {
         ].sorted(by: {$0.name < $1.name})
     }
     
+    var choir: [(name: String, view: AnyView)] {
+        [
+            ("Hayden Hofmann", AnyView(HaydenHofmann())),
+            ("Gaurav Luthria", AnyView(GauravLuthria())),
+            ("Sharon Cheng", AnyView(SharonCheng())),
+            ("Bereket Gebregziabher", AnyView(Bereket())),
+        ].sorted(by: {$0.name < $1.name})
+    }
+    
+    var budi: [(name: String, view: AnyView)] {
+        [
+            ("Zheqing (Bill) Zhu", AnyView(BillZhu())),
+            ("Taylor Lallas", AnyView(TaylorLallas())),
+            ("Tracy Cai", AnyView(TracyCai())),
+            ("Lavender Chen", AnyView(LavenderChen())),
+            ("Yoonju Kim", AnyView(YoonjuKim())),
+            ("Beste Aydin", AnyView(BesteAydin())),
+            ("Lina Fang", AnyView(LinaFang())),
+        ].sorted(by: {$0.name < $1.name})
+    }
+    
+    var activate: [(name: String, view: AnyView)] {
+        [
+            ("Alisa Wang", AnyView(AlisaWang())),
+            ("Evie Chen", AnyView(EvieChen())),
+            ("Ronit Jain", AnyView(RonitJain())),
+            ("Terresa Eun", AnyView(TerresaEun())),
+            ("Megan Worrel", AnyView(MeganWorrel())),
+            ("Savitha Srinivasan", AnyView(SavithaSrinivasan())),
+        ].sorted(by: {$0.name < $1.name})
+    }
+    
+    var gaitmate: [(name: String, view: AnyView)] {
+        [
+            ("Amita Gondi", AnyView(EmptyView())),
+            ("Laura Schütz", AnyView(LauraSchutz())),
+            ("Ankush Dhawan", AnyView(AnkushDhawan())),
+            ("Raghav Samavedam", AnyView(RaghavSamavedam())),
+        ].sorted(by: {$0.name < $1.name})
+    }
+    
+    var vasctrac2: [(name: String, view: AnyView)] {
+        [
+            ("Varun Agarwal", AnyView(VarunAgarwal()))
+        ].sorted(by: {$0.name < $1.name})
+    }
+    
     
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(submissions.indices){ submission in
-                    NavigationLink(destination: submissions[submission].view){
-                        Text(submissions[submission].name)
+        VStack {
+            
+            Text("CS342: Meet your classmates! 👋")
+            
+            NavigationView {
+                List {
+                    Section(header: Text("Team 1: CHOIR")) {
+                        ForEach(choir.indices){ submission in
+                            NavigationLink(destination: choir[submission].view){
+                                Text(choir[submission].name)
+                            }
+                        }
+                    }
+                    
+                    Section(header: Text("Team 2: BUDI")) {
+                        ForEach(budi.indices){ submission in
+                            NavigationLink(destination: budi[submission].view){
+                                Text(budi[submission].name)
+                            }
+                        }
+                    }
+                    
+                    Section(header: Text("Team 3: Activate")) {
+                        ForEach(activate.indices){ submission in
+                            NavigationLink(destination: activate[submission].view){
+                                Text(activate[submission].name)
+                            }
+                        }
+                    }
+                    
+                    Section(header: Text("Team 4: GaitMate")) {
+                        ForEach(gaitmate.indices){ submission in
+                            NavigationLink(destination: gaitmate[submission].view){
+                                Text(gaitmate[submission].name)
+                            }
+                        }
+                    }
+                    
+                    Section(header: Text("Team 5: VascTrac 2.0")) {
+                        ForEach(vasctrac2.indices){ submission in
+                            NavigationLink(destination: vasctrac2[submission].view){
+                                Text(vasctrac2[submission].name)
+                            }
+                        }
                     }
                 }
             }
